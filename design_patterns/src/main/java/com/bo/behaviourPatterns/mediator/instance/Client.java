a1ad38ee5ff7ed4b83eedb211ac25f4ed04a2346
Lenovo <gpbnice>
2021-08-23 14:38:54 +0800

    mediator

--- design_patterns/src/main/java/com/bo/behaviourPatterns/mediator/instance/Client.java
+++ design_patterns/src/main/java/com/bo/behaviourPatterns/mediator/instance/Client.java
@@ -9,8 +9,8 @@ public class Client {
     public static void main(String[] args) {
         Medium md = new EstateMedium();    //房产中介
         Customer member1, member2;
-        member1 = new Seller("张三(卖方)");
-        member2 = new Buyer("李四(买方)");
+        member1 = new Seller("马哥");
+        member2 = new Buyer("我");
         md.register(member1); //客户注册
         md.register(member2);
     }
